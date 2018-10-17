9f704781847959a75fa8f8ecf82e22523cfc0290
Noelia <noelia.grabowiecki@gmail.com>
2018-10-17 17:15:18 -0300

    Dashboard
    Login Disabled User fixed.

--- TestProject/LoginDashboardDisabledUser_test.js
+++ TestProject/LoginDashboardDisabledUser_test.js
@@ -1,9 +1,11 @@
-Feature('Login Dashboard disabled user Test');
+Feature('Login Dashboard Disabled User Test');
 
 
-Scenario('test login dashboard disabled user', (I, loginDashboardPage) => {
+Scenario('test login dashboard disabled user', (I) => {
 	I.amOnPage('https://dashboard.firstdata-test.geopagos.com/');
     I.see('Recordarme');
-    loginDashboardPage.sendForm('juana.b@firstdata.com.ar','Geopagos123.');
-    I.see('El usuario no se encuentra habilitado');
+    I.fillField('.form-control','juana.b@firstdata.com');
+    I.fillField('.login-password-input','Geopagos123.');
+    I.click('button.btn.btn-secondary.undefined');
+    I.waitForText('El usuario no se encuentra habilitado');
 });
